158f5cddd2567c780fddc0d53f7fa39f2cb881ee
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 10:56:12 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using DemoLibrary;
 
@@ -10,7 +6,7 @@ namespace ConsoleUI
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			PersonModel person = new PersonModel();
 
