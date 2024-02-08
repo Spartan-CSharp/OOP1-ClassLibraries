201034cb5b1ce1d79355b611ed78a2c58c1dea3f
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 10:54:52 -0500

    Create Code
    Create a Class Library that holds a Person Class. Use that Class in a Console Application.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DemoLibrary;
+
 namespace ConsoleUI
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			PersonModel person = new PersonModel();
+
+			Console.Write("What is your first name: ");
+			person.FirstName = Console.ReadLine();
+			Console.Write("What is your last name: ");
+			person.LastName = Console.ReadLine();
+
+			Console.WriteLine($"Hello {person.FullName}");
+
+			_ = Console.ReadLine();
 		}
 	}
 }
